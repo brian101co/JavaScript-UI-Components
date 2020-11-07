240a6bf61f96d8c89cfd0a4cee5e0decde377435
Brian Oliver <semperindoctrina19@gmail.com>
2020-11-07 01:56:00 -0600

    adjusted navbar component

--- menu/app.js
+++ menu/app.js
@@ -1,12 +1,9 @@
 class Nav {
     constructor(target, menu) {
-        this.isopen = false;
-        this.btn = target;
-        this.menu = menu;
-    }
+        if (target instanceof Element && menu instanceof Element) {
+            this.btn = target;
+            this.menu = menu;
 
-    init() {
-        if (this.btn instanceof Element && this.menu instanceof Element) {
             const self = this;
             this.btn.addEventListener('click', function () {
                 console.log("clicked");
@@ -15,6 +12,7 @@ class Nav {
         } else {
             throw "The target and Menu must be a DOM element."
         }
+        this.isopen = false;
     }
 
     open() {
@@ -31,4 +29,4 @@ class Nav {
 const navBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu-list');
 const mainNav = new Nav(navBtn, menu);
-mainNav.init();
+// mainNav.init();
